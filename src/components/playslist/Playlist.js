c4ccff9ced9a8602443ec274ab237f910c104c20
yonx44 <sanderlawrence@gmail.com>
2024-02-05 17:03:44 +0800

    Deployed website

--- src/components/playslist/Playlist.js
+++ src/components/playslist/Playlist.js
@@ -11,16 +11,15 @@ const Playlist = () => {
         <Title title="Get to know me" des="My Spotify Playlist" />
       </div>
       <iframe
-        id="playlistIframe"
-        title="Spotify Embed: Recommendation Playlist "
-        src={`https://open.spotify.com/embed/playlist/${playlistId}?utm_source=generator&theme=0`}
-        width="100%"
-        height="100%"
-        style={{ minHeight: '400px' }}
-        frameBorder="0"
-        allow="autoplay clipboard-write encrypted-media fullscreen picture-in-picture"
-        loading="lazy"
-      />
+  title="Spotify Embed: Night Drive"
+  src={`https://open.spotify.com/embed/playlist/5Cjn20ihXSv5sl3gbcFhmE?utm_source=generator&theme=0`}
+  width="100%"
+  height="100%"
+  style={{ minHeight: '420px' }}
+  frameBorder="0"
+  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+  loading="lazy"
+/>
       <section className="w-full py-10 border-b-[1px] border-b-black"></section>
     </div>
   );
