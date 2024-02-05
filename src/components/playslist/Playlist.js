df26e9ef143b709cd7118c7d30b56be430ddcee3
yonx44 <sanderlawrence@gmail.com>
2024-02-05 15:58:54 +0800

    Deployed website

--- src/components/playslist/Playlist.js
+++ src/components/playslist/Playlist.js
@@ -1,43 +1,14 @@
-import React, { useEffect } from 'react';
-import Title from '../layouts/Title'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React from 'react';
+import Title from '../layouts/Title';
 
 const Playlist = () => {
   const playlistId = '5Cjn20ihXSv5sl3gbcFhmE';
 
-  useEffect(() => {
-    const handlePlaylistLoad = () => {
-      toast.success('Spotify playlist loaded successfully!', {
-        position: 'top-right',
-        autoClose: 3000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-      });
-    };
-
-    const playlistIframe = document.getElementById('playlistIframe');
-    playlistIframe.addEventListener('load', handlePlaylistLoad);
-
-    return () => {
-      playlistIframe.removeEventListener('load', handlePlaylistLoad);
-    };
-  }, []);
-
   return (
     <div>
-      <section
-      id="playlist"
-      className="w-full py-5 border-b-[0px]"
-    ></section>  
+      <section id="playlist" className="w-full py-5 border-b-[0px]"></section>
       <div className="flex justify-center items-center text-center">
-        <Title
-          title="Get to know me"
-          des="My Spotify Playlist"
-        />
+        <Title title="Get to know me" des="My Spotify Playlist" />
       </div>
       <iframe
         id="playlistIframe"
@@ -47,13 +18,10 @@ const Playlist = () => {
         height="100%"
         style={{ minHeight: '400px' }}
         frameBorder="0"
-        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        allow="autoplay clipboard-write encrypted-media fullscreen picture-in-picture"
         loading="lazy"
       />
-      <section
-      className="w-full py-10 border-b-[1px] border-b-black"
-    ></section>  
-      <ToastContainer />
+      <section className="w-full py-10 border-b-[1px] border-b-black"></section>
     </div>
   );
 };
